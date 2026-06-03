303c89c75558d2baaa154c76675ae5bbf6818257
100RAB <115261525+100rabx23@users.noreply.github.com>
2026-06-03 11:15:42 +0000

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -4,7 +4,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_NAME = os.path.join(BASE_DIR, "database.db")
-#####
+#####2334
 
 def get_connection():
     return sqlite3.connect(DB_NAME)
